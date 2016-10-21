3fcc184b869e295f576e00740f8900bcd649da35
AbiSEH <benjamin.ableidinger@gmail.com>
2016-10-21 09:44:44 +0200

    TestRevert

--- php/updatePos.php
+++ php/updatePos.php
@@ -2,6 +2,7 @@
 	$con = mysqli_connect("localhost", "root", "", "sensSEH");
     if( !$con ) die("Error connecting to the Database: ".mysqli_connect_error() );
 
+    if($_POST['span'] == '')
 	$q = "UPDATE sensInfo SET posX =".$_POST['posX'].", posY =".$_POST['posY'].", posZ =".$_POST['posZ']." WHERE sensID =".$_POST['curSel'];
 	mysqli_query($con, $q);
 	return json_encode(array("status" => true, "added" => true));
